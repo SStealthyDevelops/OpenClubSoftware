76e45817c08af019e444228fba6c6ffc8eb67999
Satvik Mathur <satvikm05@gmail.com>
2024-08-02 21:24:44 +0530

    Added better styling to cards (fixed issue with title alignment) + created skeleton to display while club info is loading

--- app/components/ClubCard.tsx
+++ app/components/ClubCard.tsx
@@ -11,8 +11,36 @@ export type ClubInfo = {
     description?: string,
 }
 
+export function ClubCardById({id}: {id: number}) {
+    return (
+        <ClubCardSkeleton />
+    )
+}
 
-const ClubCard:React.FC<ClubInfo> = ({id, name, slug, logo, banner, description}) => {
+function ClubCardSkeleton() {
+    return (
+        <div className="flex flex-col m-4 min-h-64 h-fit card relative rounded-xl hover:scale-105 duration-200">
+            <div
+                className="relative rounded-t-xl flex flex-row bg-cover bg-center h-24 border-4 border-b-0 border-gray-500 bg-gray-700">
+                {/* GRADIENT OVERLAY */}
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-white opacity-20 rounded-t-xl"/>
+            </div>
+            <div className="flex flex-row">
+                <div className="w-fit h-fit items-start relative -mt-12 ml-5">
+                    <div className="rounded-full border-2 border-white h-[100px] w-[100px] bg-gray-600"/>
+                </div>
+                <div className="flex flex-1 justify-end">
+                    <div className="text-xl title h-8 w-32 whitespace-normal text-right mr-5 mt-3 break-words bg-gray-500 rounded-xl" />
+                </div>
+            </div>
+            <p className="m-5 mt-10 whitespace-normal text-gray-400 text-lg">
+                Loading..
+            </p>
+        </div>
+    )
+}
+
+export const ClubCard: React.FC<ClubInfo> = ({id, name, slug, logo, banner, description}) => {
 
     const router = useRouter();
 
@@ -28,19 +56,23 @@ const ClubCard:React.FC<ClubInfo> = ({id, name, slug, logo, banner, description}
         }}>
             <div className="flex flex-col m-4 min-h-64 h-fit card relative rounded-xl hover:scale-105 duration-200">
                 <div
-                    className="relative rounded-t-xl flex flex-row bg-cover bg-center h-24 border-4 border-b-0 border-gray-500" style={{backgroundImage: `url('${banner}')`}}
+                    className="relative rounded-t-xl flex flex-row bg-cover bg-center h-24 border-4 border-b-0 border-gray-500"
+                    style={{backgroundImage: `url('${banner}')`}}
                 >
                     {/* GRADIENT OVERLAY */}
-                    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-white opacity-60 rounded-t-xl" />
-            </div>
-            <div className="flex flex-row">
-                <div className="w-fit h-fit items-start relative -mt-12 ml-5">
+                    <div
+                        className="absolute inset-0 bg-gradient-to-b from-transparent to-white opacity-60 rounded-t-xl"/>
+                </div>
+                <div className="flex flex-row">
+                    <div className="w-fit h-fit items-start relative -mt-12 ml-5">
                     <Image src={logo} alt="Club Logo" height={100} width={100}
                            className="rounded-full border-4 border-white"/>
                     </div>
-                    <p className="text-xl title whitespace-normal text-right mr-3 mt-3 break-words">
-                        Extra Extra Extra Long {name}
-                    </p>
+                    <div className="flex flex-1 justify-end">
+                        <p className="text-xl title whitespace-normal text-right mr-5 mt-3 break-words">
+                            {name}
+                        </p>
+                    </div>
                 </div>
                 <p className="m-5 mt-10 whitespace-normal">
                     Lorem ipsum met dolor blah blah blah. {description}
@@ -50,4 +82,3 @@ const ClubCard:React.FC<ClubInfo> = ({id, name, slug, logo, banner, description}
     )
 }
 
-export default ClubCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,45 @@
 "use client";
 
-import ClubCard, {ClubInfo} from "@/app/components/ClubCard";
+import {ClubCard, ClubCardById, ClubInfo} from "@/app/components/ClubCard";
 import React from "react";
 import Footer from "@/app/components/Footer";
 
+/**
+ * Landing page with the grid of clubs
+ * TODO: Searchbar, Navbar, Category search (and + or search functions), Footer
+ */
 
 export default function Home() {
-  let clubs: ClubInfo[] = [];
-  for (let i = 0; i < 50; i++) {
-    clubs.push({
-      name: `Club Name ${i}`,
-      id: i,
-      slug: `club_${i}`
-    });
-  }
-  return (
-      <>
-          <main className="min-h-screen items-center justify-between m-5">
-              <div className="grid grid-cols-1 h-fit w-full md:grid-cols-2 xl:grid-cols-4 gap-1">
-                  {
-                      clubs.map((club, index) => (
-                          <ClubCard {...club} key={index}/>
-                      ))
-                  }
-              </div>
-          </main>
-          <Footer/>
-      </>
-  );
+
+    return (
+        <>
+            <ClubGrid />
+            <Footer/>
+        </>
+)
+}
+
+function ClubGrid() {
+    let clubs: ClubInfo[] = [];
+    for (let i = 0; i < 50; i++) {
+        clubs.push({
+            name: `Club Name ${i}`,
+            id: i,
+            slug: `club_${i}`
+        });
+    }
+    return (
+        <>
+            <main className="min-h-screen items-center justify-between m-5">
+                <div className="grid grid-cols-1 h-fit w-full md:grid-cols-2 xl:grid-cols-4 gap-1">
+                    {
+                        clubs.map((club, index) => (
+                            <ClubCardById id={club.id} key={index}/>
+                            // <ClubCard key={index} {...club} />
+                        ))
+                    }
+                </div>
+            </main>
+        </>
+    );
 }
