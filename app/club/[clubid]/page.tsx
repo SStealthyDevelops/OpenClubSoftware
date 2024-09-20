96fdf29941a2f538027834fba9acebbc0f7ece73
Satvik Mathur <satvikm05@gmail.com>
2024-09-20 13:57:57 -0400

    Added license

--- app/club/[clubid]/page.tsx
+++ app/club/[clubid]/page.tsx
@@ -1,7 +1,10 @@
-import {notFound} from "next/navigation";
+import {createClient} from "@/app/utils/supabase/server";
 
 export default function ClubPage({ params }: { params: { clubid: string } }){
     // Find club data from database -- if not found, use notFound();
+    const supabase = createClient();
+
+    // const data = supabase.from('')
     return (
         <div>
             {params.clubid}
